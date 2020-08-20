33874c5cc498c0b57ed0146f1b5712d53b92a9c0
tescales <toscal@microsoft.com>
2020-08-20 12:22:24 -0700

    update bootstrap.sh script

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,39 +1,8 @@
 #!/bin/bash
 # additional environment variables available: $AZURE_SUBSCRIPTION_ID, $AZURE_AADTENANT_ID and $AZURE_KEYVAULT
 
-echo Location: $AZURE_LOCATION
+echo Connecting to Azure Storage: $AZURE_STORAGE_ACCOUNT
 echo Resource Group: $AZURE_RESOURCE_GROUP
-
-# cribbed from http://fahdshariff.blogspot.com/2014/02/retrying-commands-in-shell-scripts.html
-# Retries a command on failure. 
-# $1 - the max number of attempts
-# $2... - the command to run
-
-retry() {
-    local -r -i max_attempts="$1"; shift
-    local -r cmd="$@"
-    local -i attempt_num=1
-    until $cmd
-    do
-        if ((attempt_num==max_attempts))
-        then
-            echo "Attempt $attempt_num failed and there are no more attempts left!"
-            return 1
-        else
-            echo "Attempt $attempt_num failed! Trying again in $attempt_num seconds..."
-            sleep $((attempt_num++))
-        fi
-    done
-}
-
-retry 5 az login --identity
-
-az configure --defaults location=$AZURE_LOCATION
-az configure --defaults group=$AZURE_RESOURCE_GROUP
-
-cd /$BOOTSTRAP_REPO
-
-### Custom Code goes here 
 
 #this function escapes the passed-in path to work with sed
 function sedPath {  
